f560cf0d5d2083b5ac1739ef970ae4f61c80a004
anukshaa <167706994+anukshaa@users.noreply.github.com>
2025-06-02 17:24:28 +0530

    Create pillow.py

---
+++ pillow.py
@@ -0,0 +1,4 @@
+from PIL import Image
+# Load an example image (replace with an actual path to an image file)
+img = Image.open('C:\Users\Anshika\OneDrive\Desktop\mood_music_recommendation\fer2013.zip\train\angry\Training_3908.jpg')
+img.show()
